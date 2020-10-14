1c2b4aa6b01657ddf450c761d87099f69b68a486
John Leavell <johnleavell@gmail.com>
2020-10-14 16:59:37 -0500

    added home and about routes and links to router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import { Drawer, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core'
+import { Drawer, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import HomeIcon from '@material-ui/icons/Home';
+import InfoIcon from '@material-ui/icons/Info';
 
 const useStyles = makeStyles((theme) => ({
-  drawerPaper: { width: 'inherit'}
+  drawerPaper: { width: 'inherit'},
+  link: { textDecoration: 'none', color: theme.palette.text.primary}
 }))
 
 function App() {
@@ -20,8 +23,27 @@ function App() {
         open={true}
         classes={{ paper: classes.drawerPaper }}
       >
+        <List>
+          <Link to='/' className={classes.link}>
+          <ListItem button>
+            <ListItemIcon>
+              <HomeIcon/>
+            </ListItemIcon>
+            <ListItemText primary="Home"/ >
+          </ListItem>
+          </Link>
+          <Link to='/about' className={classes.link}>
+          <ListItem button>
+            <ListItemIcon>
+              <InfoIcon />
+            </ListItemIcon>
+            <ListItemText primary="About"/ >
+          </ListItem>
+          </Link>
+        </List>
         
       </Drawer>
+      
 
       </div>
     </Router>
